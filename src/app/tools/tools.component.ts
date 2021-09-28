c3bed23a4a260aea672853fb7d9a1e0b73dfabd2
muahmmadfarhan <m.farhankz@gmail.com>
2021-09-28 13:47:23 +0500

    new screen tree view styling v3

--- src/app/tools/tools.component.ts
+++ src/app/tools/tools.component.ts
@@ -13,10 +13,12 @@ export class ToolsComponent implements OnInit {
       "id": "7340",
       "label": "Documents",
       "data": "Documents Folder",
+      expanded: true,
       "children": [
         {
         "label": "Work",
         "data": "Work Folder",
+          expanded: true,
         "children": [{"label": "Expenses.doc", "icon": "", "data": "Expenses Document"}, {"label": "Resume.doc", "icon": "", "data": "Resume Document"}]
         },
         {
@@ -29,6 +31,7 @@ export class ToolsComponent implements OnInit {
       "id": "7341",
       "label": "Pictures",
       "data": "Pictures Folder",
+      expanded: true,
       "children": [
         {"label": "barcelona.jpg", "icon": "", "data": "Barcelona Photo"},
         {"label": "logo.jpg", "icon": "", "data": "PrimeFaces Logo"},
@@ -36,7 +39,112 @@ export class ToolsComponent implements OnInit {
     },
     {
       "id": "7342",
-      "label": "Movies",
+      "label": "Components",
+      "data": "Movies Folder",
+      "children": [{
+        "label": "Al Pacino",
+        "data": "Pacino Movies",
+        "children": [{"label": "Scarface", "icon": "", "data": "Scarface Movie"}, {"label": "Serpico", "icon": "", "data": "Serpico Movie"}]
+      },
+        {
+          "label": "Robert De Niro",
+          "data": "De Niro Movies",
+          "children": [{"label": "Goodfellas", "icon": "", "data": "Goodfellas Movie"}, {"label": "Untouchables", "icon": "", "data": "Untouchables Movie"}]
+        }]
+    },
+    {
+      "id": "7343",
+      "label": "Modals",
+      "data": "Movies Folder",
+      "children": [{
+        "label": "Al Pacino",
+        "data": "Pacino Movies",
+        "children": [{"label": "Scarface", "icon": "", "data": "Scarface Movie"}, {"label": "Serpico", "icon": "", "data": "Serpico Movie"}]
+      },
+        {
+          "label": "Robert De Niro",
+          "data": "De Niro Movies",
+          "children": [{"label": "Goodfellas", "icon": "", "data": "Goodfellas Movie"}, {"label": "Untouchables", "icon": "", "data": "Untouchables Movie"}]
+        }]
+    },
+    {
+      "id": "7344",
+      "label": "Widgets",
+      "data": "Movies Folder",
+      "children": [{
+        "label": "Al Pacino",
+        "data": "Pacino Movies",
+        "children": [{"label": "Scarface", "icon": "", "data": "Scarface Movie"}, {"label": "Serpico", "icon": "", "data": "Serpico Movie"}]
+      },
+        {
+          "label": "Robert De Niro",
+          "data": "De Niro Movies",
+          "children": [{"label": "Goodfellas", "icon": "", "data": "Goodfellas Movie"}, {"label": "Untouchables", "icon": "", "data": "Untouchables Movie"}]
+        }]
+    },
+    {
+      "id": "7345",
+      "label": "Angular",
+      "data": "Movies Folder",
+      "children": [{
+        "label": "Al Pacino",
+        "data": "Pacino Movies",
+        "children": [{"label": "Scarface", "icon": "", "data": "Scarface Movie"}, {"label": "Serpico", "icon": "", "data": "Serpico Movie"}]
+      },
+        {
+          "label": "Robert De Niro",
+          "data": "De Niro Movies",
+          "children": [{"label": "Goodfellas", "icon": "", "data": "Goodfellas Movie"}, {"label": "Untouchables", "icon": "", "data": "Untouchables Movie"}]
+        }]
+    },
+    {
+      "id": "7346",
+      "label": "React Native",
+      "data": "Movies Folder",
+      "children": [{
+        "label": "Al Pacino",
+        "data": "Pacino Movies",
+        "children": [{"label": "Scarface", "icon": "", "data": "Scarface Movie"}, {"label": "Serpico", "icon": "", "data": "Serpico Movie"}]
+      },
+        {
+          "label": "Robert De Niro",
+          "data": "De Niro Movies",
+          "children": [{"label": "Goodfellas", "icon": "", "data": "Goodfellas Movie"}, {"label": "Untouchables", "icon": "", "data": "Untouchables Movie"}]
+        }]
+    },
+    {
+      "id": "7347",
+      "label": "Flutter",
+      "data": "Movies Folder",
+      "children": [{
+        "label": "Al Pacino",
+        "data": "Pacino Movies",
+        "children": [{"label": "Scarface", "icon": "", "data": "Scarface Movie"}, {"label": "Serpico", "icon": "", "data": "Serpico Movie"}]
+      },
+        {
+          "label": "Robert De Niro",
+          "data": "De Niro Movies",
+          "children": [{"label": "Goodfellas", "icon": "", "data": "Goodfellas Movie"}, {"label": "Untouchables", "icon": "", "data": "Untouchables Movie"}]
+        }]
+    },
+    {
+      "id": "7348",
+      "label": "Node Js",
+      "data": "Movies Folder",
+      "children": [{
+        "label": "Al Pacino",
+        "data": "Pacino Movies",
+        "children": [{"label": "Scarface", "icon": "", "data": "Scarface Movie"}, {"label": "Serpico", "icon": "", "data": "Serpico Movie"}]
+      },
+        {
+          "label": "Robert De Niro",
+          "data": "De Niro Movies",
+          "children": [{"label": "Goodfellas", "icon": "", "data": "Goodfellas Movie"}, {"label": "Untouchables", "icon": "", "data": "Untouchables Movie"}]
+        }]
+    },
+    {
+      "id": "7349",
+      "label": "MongoDb",
       "data": "Movies Folder",
       "children": [{
         "label": "Al Pacino",
