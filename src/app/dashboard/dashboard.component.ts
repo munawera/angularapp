d7cfac98a8f687d0f9b112d652e12f020719db88
muahmmadfarhan <m.farhankz@gmail.com>
2021-10-14 23:46:41 +0500

    dashboard done

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -67,8 +67,80 @@ export class DashboardComponent implements OnInit {
       created: '26 Apr 2019',
       created_by: 'CRM project'
     },
+    {
+      created: '22 Apr 2019',
+      created_by: 'Dashboard project'
+    },
+    {
+      created: '26 Apr 2019',
+      created_by: 'CRM project'
+    },
+    {
+      created: '22 Apr 2019',
+      created_by: 'Dashboard project'
+    },
+    {
+      created: '26 Apr 2019',
+      created_by: 'CRM project'
+    },
+    {
+      created: '22 Apr 2019',
+      created_by: 'Dashboard project'
+    },
+    {
+      created: '26 Apr 2019',
+      created_by: 'CRM project'
+    },
+    {
+      created: '22 Apr 2019',
+      created_by: 'Dashboard project'
+    },
+    {
+      created: '26 Apr 2019',
+      created_by: 'CRM project'
+    },
   ];
   review = [
+    {
+      id: 654,
+      title: "Test review",
+      description: "User name and password",
+      category: 'Umar',
+      created: '12/04/2021',
+      created_by: 'Abu Baker'
+    },
+    {
+      id: 655,
+      title: "Test review",
+      description: "User name and password",
+      category: 'Umar',
+      created: '12/04/2021',
+      created_by: 'Abu Baker'
+    },
+    {
+      id: 656,
+      title: "Test review",
+      description: "User name and password",
+      category: 'Umar',
+      created: '12/04/2021',
+      created_by: 'Abu Baker'
+    },
+    {
+      id: 657,
+      title: "Test review",
+      description: "User name and password",
+      category: 'Umar',
+      created: '12/04/2021',
+      created_by: 'Abu Baker'
+    },
+    {
+      id: 658,
+      title: "Test review",
+      description: "User name and password",
+      category: 'Umar',
+      created: '12/04/2021',
+      created_by: 'Abu Baker'
+    },
     {
       id: 654,
       title: "Test review",
