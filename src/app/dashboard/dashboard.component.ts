a741d433fbcf4a9dc1f51a0072f42b4bbd66431a
muahmmadfarhan <m.farhankz@gmail.com>
2021-10-14 23:25:04 +0500

    dashboard almost done

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -68,6 +68,49 @@ export class DashboardComponent implements OnInit {
       created_by: 'CRM project'
     },
   ];
+  review = [
+    {
+      id: 654,
+      title: "Test review",
+      description: "User name and password",
+      category: 'Umar',
+      created: '12/04/2021',
+      created_by: 'Abu Baker'
+    },
+    {
+      id: 655,
+      title: "Test review",
+      description: "User name and password",
+      category: 'Umar',
+      created: '12/04/2021',
+      created_by: 'Abu Baker'
+    },
+    {
+      id: 656,
+      title: "Test review",
+      description: "User name and password",
+      category: 'Umar',
+      created: '12/04/2021',
+      created_by: 'Abu Baker'
+    },
+    {
+      id: 657,
+      title: "Test review",
+      description: "User name and password",
+      category: 'Umar',
+      created: '12/04/2021',
+      created_by: 'Abu Baker'
+    },
+    {
+      id: 658,
+      title: "Test review",
+      description: "User name and password",
+      category: 'Umar',
+      created: '12/04/2021',
+      created_by: 'Abu Baker'
+    },
+
+  ];
   constructor() { }
 
   ngOnInit(): void {
